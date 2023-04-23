5da0b2c48a7ee640ae8e55f6d3cef9fba42e8846
Giorgio Pomettini <giorgio.freespire@gmail.com>
2023-04-23 17:56:59 +0200

    Refactored code, added minifb to binary and made IBM demo running

--- src/chip8.rs
+++ src/chip8.rs
@@ -34,51 +34,53 @@ impl Chip8 {
     }
 
     pub fn load_rom(&mut self, buf: &[u8]) {
-        for i in 0..buf.len() {
-            self.memory[i + ENTRY_POINT] = buf[i];
-        }
+        self.memory[ENTRY_POINT..(buf.len() + ENTRY_POINT)].copy_from_slice(buf);
     }
 
     pub fn tick(&mut self) {
         let opcode = (self.memory[self.pc as usize] as u16) << 8
             | (self.memory[self.pc as usize + 1] as u16);
 
-        // println!("{:#04x}", opcode);
+        let nib_1 = (opcode & 0xF000) >> 12;
+        let nib_2 = (opcode & 0x0F00) >> 8;
+        let nib_3 = (opcode & 0x00F0) >> 4;
+        let nib_4 = opcode & 0x000F;
 
-        match opcode & 0xF000 {
-            // 00E_
-            0x0000 => {
-                match opcode & 0x000F {
-                    // 00E0 - Clear screen
-                    0x0000 => {
-                        self.gfx_buffer = [0; SCREEN_WIDTH * SCREEN_HEIGHT];
-                        self.pc += 2;
-                    }
-                    // 00EE - Return from subroutine
-                    0x000E => {
-                        self.sp -= 1;
-                        self.pc = self.stack[self.sp as usize];
-                        self.pc += 2;
-                    }
-                    _ => panic!("Unknown opcode"),
-                }
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
+        let nnn = opcode & 0x0FFF;
+        let kk = (opcode & 0x00FF) as u8;
+        let n = (opcode & 0x000F) as u8;
+
+        match (nib_1, nib_2, nib_3, nib_4) {
+            // 00E0 - Clear screen
+            (0, 0, 0xE, 0) => {
+                self.gfx_buffer = [0; SCREEN_WIDTH * SCREEN_HEIGHT];
+                self.pc += 2;
+            }
+
+            // 00EE - Return from subroutine
+            (0, 0, 0xE, 0xE) => {
+                self.sp -= 1;
+                self.pc = self.stack[self.sp as usize];
+                self.pc += 2;
             }
 
             // 1NNN - Jumps to address NNN
-            0x1000 => {
-                self.pc = opcode & 0x0FFF;
+            (0x1, _, _, _) => {
+                self.pc = nnn;
             }
 
             // 2NNN - Calls subroutine at NNN
-            0x2000 => {
+            (0x2, _, _, _) => {
                 self.stack[self.sp as usize] = self.pc;
                 self.sp += 1;
-                self.pc = opcode & 0x0FFF;
+                self.pc = nnn;
             }
 
             // 3XNN - Skips the next instruction if VX equals NN.
-            0x3000 => {
-                if self.v[((opcode & 0x0F00) >> 8) as usize] as u16 == (opcode & 0x00FF) {
+            (0x3, _, _, _) => {
+                if self.v[x] as u16 == (opcode & 0x00FF) {
                     self.pc += 4;
                 } else {
                     self.pc += 2;
@@ -86,61 +88,60 @@ impl Chip8 {
             }
 
             // 4XNN - Skips the next instruction if VX does not equal NN.
-            0x4000 => {
+            (0x4, _, _, _) => {
                 todo!()
             }
 
             // 5XY0 - Skips the next instruction if VX equals VY.
-            0x5000 => {
+            (0x5, _, _, _) => {
                 todo!()
             }
 
             // 6XNN - Sets VX to NN.
-            0x6000 => {
-                self.v[((opcode & 0x0F00) >> 8) as usize] = (opcode & 0x00FF) as u8;
+            (0x6, _, _, _) => {
+                self.v[x] = kk;
                 self.pc += 2;
             }
 
             // 7XNN - Adds NN to VX.
-            0x7000 => {
-                self.v[((opcode & 0x0F00) >> 8) as usize] += (opcode & 0x00FF) as u8;
+            (0x7, _, _, _) => {
+                self.v[x] += kk;
                 self.pc += 2;
             }
 
             // 8XY_
-            0x8000 => {
+            (0x8, _, _, 0x0) => {
                 todo!()
             }
 
             // 9XY0 - Skips the next instruction if VX doesn't equal VY
-            0x9000 => {
+            (0x9, _, _, _) => {
                 todo!()
             }
 
             // ANNN - Sets I to the address NNN.
-            0xA000 => {
-                self.i = opcode & 0x0FFF;
+            (0xA, _, _, _) => {
+                self.i = nnn;
                 self.pc += 2;
             }
 
             // BNNN - Jumps to the address NNN plus V0.
-            0xB000 => {
-                self.pc = (opcode & 0x0FFF) + self.v[0] as u16;
+            (0xB, _, _, _) => {
+                self.pc = (nnn) + self.v[0] as u16;
                 self.pc += 2;
             }
 
             // CXNN - Sets VX to a random number, masked by NN.
-            0xC000 => {
+            (0xC, _, _, _) => {
                 // TODO: Needs to be tested
-                self.v[((opcode & 0x0F00) >> 8) as usize] =
-                    (rand::random::<u8>() % 0xFF + 1) & (opcode & 0x00FF) as u8;
+                self.v[x] = (rand::random::<u8>() % 0xFF + 1) & kk;
                 self.pc += 2;
             }
 
             // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
-            0xD000 => {
-                let vx = self.v[((opcode & 0x0F00) >> 8) as usize] as u16;
-                let vy = self.v[((opcode & 0x00F0) >> 4) as usize] as u16;
+            (0xD, _, _, _) => {
+                let vx = self.v[x] as u16;
+                let vy = self.v[y] as u16;
                 let height = opcode & 0x000F;
                 self.v[0xF] &= 0;
 
@@ -160,65 +161,62 @@ impl Chip8 {
             }
 
             // EX__
-            0xE000 => {
+            (0xE, _, 0x9, 0xE) => {
                 todo!()
             }
 
-            // FX__
-            0xF000 => match opcode & 0x00FF {
-                // FX07 - Sets VX to the value of the delay timer
-                0x0007 => {
-                    self.v[((opcode & 0x0F00) >> 8) as usize] = self.dt;
-                    self.pc += 2;
-                }
-                // FX0A - A key press is awaited, and then stored in VX
-                0x000A => {
-                    todo!()
-                }
-                // FX15 - Sets the delay timer to VX
-                0x0015 => {
-                    self.dt = self.v[((opcode & 0x0F00) >> 8) as usize];
-                    self.pc += 2;
-                }
-                // FX18 - Sets the sound timer to VX
-                0x0018 => {
-                    todo!()
-                }
-                // FX1E - Adds VX to I
-                0x001E => {
-                    todo!()
-                }
-                // FX29 - Sets I to the location of the sprite for the character in VX.
-                0x0029 => {
-                    self.i = self.v[((opcode & 0x0F00) >> 8) as usize] as u16 * 0x5;
-                    self.pc += 2;
-                }
-                // FX33 - Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
-                0x0033 => {
-                    self.memory[self.i as usize] = self.v[((opcode & 0x0F00) >> 8) as usize] / 100;
-                    self.memory[(self.i + 1) as usize] =
-                        (self.v[((opcode & 0x0F00) >> 8) as usize] / 10) % 10;
-                    self.memory[(self.i + 2) as usize] =
-                        self.v[((opcode & 0x0F00) >> 8) as usize] % 10;
-                    self.pc += 2;
-                }
-                // FX55 - Stores V0 to VX in memory starting at address I
-                0x0055 => {
-                    todo!()
-                }
-                0x0065 => {
-                    // TODO: Must be tested
-                    for i in 0..((opcode & 0x0F00) >> 8) {
-                        self.v[i as usize] = self.memory[(self.i + i) as usize];
-                    }
+            // FX07 - Sets VX to the value of the delay timer
+            (0xF, _, 0x0, 0x7) => {
+                self.v[x] = self.dt;
+                self.pc += 2;
+            }
 
-                    self.i += ((opcode & 0x0F00) >> 8) + 1;
-                    self.pc += 2;
-                }
-                _ => todo!(),
-            },
+            // FX0A - A key press is awaited, and then stored in VX
+            (0xF, _, 0x0, 0xA) => {
+                todo!()
+            }
 
-            _ => panic!("Unknown opcode"),
+            // FX15 - Sets the delay timer to VX
+            (0xF, _, 0x1, 0x5) => {
+                self.dt = self.v[x];
+                self.pc += 2;
+            }
+
+            // FX1E - Adds VX to I
+            (0xF, _, 0x1, 0xE) => {
+                todo!()
+            }
+
+            // FX29 - Sets I to the location of the sprite for the character in VX.
+            (0xF, _, 0x2, 0x9) => {
+                self.i = self.v[x] as u16 * 0x5;
+                self.pc += 2;
+            }
+
+            // FX33 - Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
+            (0xF, _, 0x3, 0x3) => {
+                self.memory[self.i as usize] = self.v[x] / 100;
+                self.memory[(self.i + 1) as usize] = (self.v[x] / 10) % 10;
+                self.memory[(self.i + 2) as usize] = self.v[x] % 10;
+                self.pc += 2;
+            }
+
+            // FX55 - Stores V0 to VX in memory starting at address I
+            (0xF, _, 0x5, 0x5) => {
+                todo!()
+            }
+
+            (0xF, _, 0x6, 0x5) => {
+                // TODO: Must be tested
+                for i in 0..((opcode & 0x0F00) >> 8) {
+                    self.v[i as usize] = self.memory[(self.i + i) as usize];
+                }
+
+                self.i += ((opcode & 0x0F00) >> 8) + 1;
+                self.pc += 2;
+            }
+
+            (_, _, _, _) => panic!("Unknown opcode"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-// extern crate sdl2;
+extern crate minifb;
 
 mod chip8;
 
 use chip8::Chip8;
 use std::{fs::File, io::Read};
 
-/*
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use std::time::Duration;
-*/
+use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
+
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
 
 fn main() {
     let mut file = File::open("roms/ibm-logo.rom").unwrap();
@@ -21,51 +19,36 @@ fn main() {
     let mut cpu = Chip8::new();
     cpu.load_rom(&buf);
 
-    for _ in 0..10000 {
+    let mut window = Window::new(
+        "Yet Another Chip-8 Emulator",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            borderless: false,
+            transparency: false,
+            title: true,
+            resize: true,
+            scale: Scale::X16,
+            scale_mode: ScaleMode::AspectRatioStretch,
+            topmost: false,
+            none: false,
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    let mut buffer;
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
         cpu.tick();
+
+        buffer = cpu.gfx_buffer.map(|x| (x as u32) * u32::MAX);
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
-
-    println!("{:?}", &cpu.gfx_buffer);
-
-    /*
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .map_err(|e| e.to_string())
-        .unwrap();
-
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .map_err(|e| e.to_string())
-        .unwrap();
-
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-
-        canvas.clear();
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
-    }
-    */
 }
