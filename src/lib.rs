d71f7d70fa5ade707aa46c2d377060e06471a8aa
Giorgio Pomettini <giorgio.freespire@gmail.com>
2023-05-08 17:09:37 +0200

    More opcode refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -162,6 +162,8 @@ impl Chip8 {
         self.st == 1
     }
 
+    // Start opcodes
+
     fn cls(&mut self) {
         // 00E0 - Clear screen
         self.gfx_buffer = [false; SCREEN_SIZE];
@@ -215,7 +217,125 @@ impl Chip8 {
         }
     }
 
-    // ---
+    fn ld_vx_byte(&mut self, x: u8, kk: u8) {
+        // 6XNN - Sets VX to NN.
+        self.v[x as usize] = kk;
+        self.pc += 2;
+    }
+
+    fn add_vx_byte(&mut self, x: u8, kk: u8) {
+        // 7XNN - Adds NN to VX.
+        self.v[x as usize] += kk;
+        self.pc += 2;
+    }
+
+    fn ld_vx_vy(&mut self, x: u8, y: u8) {
+        // 8XY0 - Sets VX to the value of VY.
+        self.v[x as usize] = self.v[y as usize];
+        self.pc += 2;
+    }
+
+    fn or_vx_vy(&mut self, x: u8, y: u8) {
+        // 8XY1 - Sets VX to (VX OR VY).
+        self.v[x as usize] |= self.v[y as usize];
+        self.pc += 2;
+    }
+
+    fn and_vx_vy(&mut self, x: u8, y: u8) {
+        // 8XY2 - Sets VX to (VX AND VY).
+        self.v[x as usize] &= self.v[y as usize];
+        self.pc += 2;
+    }
+
+    fn xor_vx_vy(&mut self, x: u8, y: u8) {
+        // 8XY3 - Sets VX to (VX XOR VY).
+        self.v[x as usize] ^= self.v[y as usize];
+        self.pc += 2;
+    }
+
+    fn add_vx_vy(&mut self, x: u8, y: u8) {
+        // 8XY4 - Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+        self.v[x as usize] += self.v[y as usize];
+
+        if self.v[y as usize] > (0xFF - self.v[x as usize]) {
+            self.v[0xF] = 1;
+        } else {
+            self.v[0xF] = 0;
+        }
+
+        self.pc += 2;
+    }
+
+    fn sub_vx_vy(&mut self, x: u8, y: u8) {
+        // 8XY5 - VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+        if self.v[y as usize] > self.v[x as usize] {
+            self.v[0xF] = 0;
+        } else {
+            self.v[0xF] = 1;
+        }
+
+        self.v[x as usize] -= self.v[y as usize];
+        self.pc += 2;
+    }
+
+    fn shr_vx_vy(&mut self, x: u8) {
+        // 0x8XY6 - Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
+        self.v[0xF] = self.v[x as usize] & 0x1;
+        self.v[x as usize] >>= 1;
+        self.pc += 2;
+    }
+
+    fn subn_vx_vy(&mut self, x: u8, y: u8) {
+        // 0x8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+        if self.v[x as usize] > self.v[y as usize] {
+            self.v[0xF] = 0;
+        } else {
+            self.v[0xF] = 1;
+        }
+
+        self.v[x as usize] = self.v[y as usize] - self.v[x as usize];
+        self.pc += 2;
+    }
+
+    fn shl_vx_vy(&mut self, x: u8) {
+        // 0x8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.
+        self.v[0xF] = self.v[x as usize] >> 7;
+        self.v[x as usize] <<= 1;
+        self.pc += 2;
+    }
+
+    fn sne_vx_vy(&mut self, x: u8, y: u8) {
+        // 9XY0 - Skips the next instruction if VX doesn't equal VY
+        if self.v[x as usize] != self.v[y as usize] {
+            self.pc += 4;
+        } else {
+            self.pc += 2;
+        }
+    }
+
+    fn ld_i_addr(&mut self, nnn: u16) {
+        // ANNN - Sets I to the address NNN.
+        self.i = nnn;
+        self.pc += 2;
+    }
+
+    fn jp_v0_addr(&mut self, nnn: u16) {
+        // BNNN - Jumps to the address NNN plus V0.
+        self.pc = nnn + self.v[0] as u16;
+        self.pc += 2;
+    }
+
+    fn rnd_vx_byte(&mut self, x: u8, kk: u8) {
+        // CXNN - Sets VX to a random number, masked by NN.
+        match self.rnd_seed {
+            Some(ref mut seed) => {
+                self.v[x as usize] = (seed.next_u32() as u8 % 0xFF + 1) & kk;
+            }
+            None => self.v[x as usize] = 1 & kk,
+        }
+
+        self.pc += 2;
+    }
 
     fn drw_vx_vy_nibble(&mut self, x: u8, y: u8, opcode: u16) {
         // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
@@ -252,10 +372,10 @@ impl Chip8 {
         self.pc += 2;
     }
 
+    // End opcodes
+
     fn tick(&mut self) {
         let opcode = self.get_opcode();
-
-        // dbg!(opcode);
 
         let nib_1 = (opcode & 0xF000) >> 12;
         let nib_2 = (opcode & 0x0F00) >> 8;
@@ -282,129 +402,21 @@ impl Chip8 {
             (0x3, _, _, _) => self.se_vx_byte(x, kk),
             (0x4, _, _, _) => self.sne_vx_byte(x, kk),
             (0x5, _, _, _) => self.se_vx_vy(x, y),
-
-            // 6XNN - Sets VX to NN.
-            (0x6, _, _, _) => {
-                self.v[x as usize] = kk;
-                self.pc += 2;
-            }
-
-            // 7XNN - Adds NN to VX.
-            (0x7, _, _, _) => {
-                self.v[x as usize] += kk;
-                self.pc += 2;
-            }
-
-            // 8XY0 - Sets VX to the value of VY.
-            (0x8, _, _, 0x0) => {
-                self.v[x as usize] = self.v[y as usize];
-                self.pc += 2;
-            }
-
-            // 8XY1 - Sets VX to (VX OR VY).
-            (0x8, _, _, 0x1) => {
-                self.v[x as usize] |= self.v[y as usize];
-                self.pc += 2;
-            }
-
-            // 8XY2 - Sets VX to (VX AND VY).
-            (0x8, _, _, 0x2) => {
-                self.v[x as usize] &= self.v[y as usize];
-                self.pc += 2;
-            }
-
-            // 8XY3 - Sets VX to (VX XOR VY).
-            (0x8, _, _, 0x3) => {
-                self.v[x as usize] ^= self.v[y as usize];
-                self.pc += 2;
-            }
-
-            // 8XY4 - Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
-            (0x8, _, _, 0x4) => {
-                self.v[x as usize] += self.v[y as usize];
-
-                if self.v[y as usize] > (0xFF - self.v[x as usize]) {
-                    self.v[0xF] = 1;
-                } else {
-                    self.v[0xF] = 0;
-                }
-
-                self.pc += 2;
-            }
-
-            // 8XY5 - VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
-            (0x8, _, _, 0x5) => {
-                if self.v[y as usize] > self.v[x as usize] {
-                    self.v[0xF] = 0;
-                } else {
-                    self.v[0xF] = 1;
-                }
-
-                self.v[x as usize] -= self.v[y as usize];
-                self.pc += 2;
-            }
-
-            // 0x8XY6 - Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
-            (0x8, _, _, 0x6) => {
-                self.v[0xF] = self.v[x as usize] & 0x1;
-                self.v[x as usize] >>= 1;
-                self.pc += 2;
-            }
-
-            // 0x8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
-            (0x8, _, _, 0x7) => {
-                if self.v[x as usize] > self.v[y as usize] {
-                    self.v[0xF] = 0;
-                } else {
-                    self.v[0xF] = 1;
-                }
-
-                self.v[x as usize] = self.v[y as usize] - self.v[x as usize];
-                self.pc += 2;
-            }
-
-            // 0x8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.
-            (0x8, _, _, 0xE) => {
-                self.v[0xF] = self.v[x as usize] >> 7;
-                self.v[x as usize] <<= 1;
-                self.pc += 2;
-            }
-
-            // 9XY0 - Skips the next instruction if VX doesn't equal VY
-            (0x9, _, _, _) => {
-                if self.v[x as usize] != self.v[y as usize] {
-                    self.pc += 4;
-                } else {
-                    self.pc += 2;
-                }
-            }
-
-            // ANNN - Sets I to the address NNN.
-            (0xA, _, _, _) => {
-                self.i = nnn;
-                self.pc += 2;
-            }
-
-            // BNNN - Jumps to the address NNN plus V0.
-            (0xB, _, _, _) => {
-                self.pc = nnn + self.v[0] as u16;
-                self.pc += 2;
-            }
-
-            // CXNN - Sets VX to a random number, masked by NN.
-            (0xC, _, _, _) => {
-                // TODO: Needs a random number
-                match self.rnd_seed {
-                    Some(ref mut seed) => {
-                        self.v[x as usize] = (seed.next_u32() as u8 % 0xFF + 1) & kk;
-                    }
-                    None => self.v[x as usize] = 1 & kk,
-                }
-                // self.v[x as usize] = (RAND % 0xFF + 1) & kk;
-                // self.v[x as usize] = 1 & kk;
-                self.pc += 2;
-            }
-
+            (0x6, _, _, _) => self.ld_vx_byte(x, kk),
+            (0x7, _, _, _) => self.add_vx_byte(x, kk),
+            (0x8, _, _, 0x0) => self.ld_vx_vy(x, y),
+            (0x8, _, _, 0x1) => self.or_vx_vy(x, y),
+            (0x8, _, _, 0x2) => self.and_vx_vy(x, y),
+            (0x8, _, _, 0x3) => self.xor_vx_vy(x, y),
+            (0x8, _, _, 0x4) => self.add_vx_vy(x, y),
+            (0x8, _, _, 0x5) => self.sub_vx_vy(x, y),
+            (0x8, _, _, 0x6) => self.shr_vx_vy(x),
+            (0x8, _, _, 0x7) => self.subn_vx_vy(x, y),
+            (0x8, _, _, 0xE) => self.shl_vx_vy(x),
+            (0x9, _, _, _) => self.sne_vx_vy(x, y),
+            (0xA, _, _, _) => self.ld_i_addr(nnn),
+            (0xB, _, _, _) => self.jp_v0_addr(nnn),
+            (0xC, _, _, _) => self.rnd_vx_byte(x, kk),
             (0xD, _, _, _) => self.drw_vx_vy_nibble(x, y, opcode),
 
             // EX9E - Skips the next instruction if the key stored in VX is pressed.
@@ -501,12 +513,8 @@ impl Chip8 {
                 self.pc += 2;
             }
 
-            // FX75
-            (0xF, _, 0x7, 0x5) => unimplemented!("LD R, Vx"),
-
-            // FX85
-            (0xF, _, 0x8, 0x5) => unimplemented!("LD Vx, R"),
-
+            (0xF, _, 0x7, 0x5) => unimplemented!("LD R, Vx"), // FX75
+            (0xF, _, 0x8, 0x5) => unimplemented!("LD Vx, R"), // FX85
             (0xF, _, 0x6, 0x5) => self.ld_vx_i(x),
             (_, _, _, _) => panic!("Unknown opcode: {opcode:#04X}"),
         }
