cc55dd5af54dfc01a89e683846003ffbe726665c
Giorgio Pomettini <giorgio.freespire@gmail.com>
2024-01-15 19:06:24 +0100

    Updated Playdate build

--- playdate/crankstart/README.md
+++ playdate/crankstart/README.md
@@ -54,9 +54,8 @@ crate-type = ["staticlib", "cdylib"]
 crankstart = { path = "../crankstart" }
 crankstart-sys = { path = "../crankstart/crankstart-sys" }
 anyhow = { version = "1.0.31", default-features = false }
-euclid = { version = "0.20.13", default-features = false, features = [ "libm" ] }
-hashbrown = "0.7.2"
-heapless = "0.5.5"
+euclid = { version = "0.22.9", default-features = false, features = [ "libm" ] }
+hashbrown = "0.14.0"
 
 [dependencies.cstr_core]
 version = "=0.1.2"
@@ -79,3 +78,17 @@ features = [ "alloc" ]
 7. To run the project, from its root, you should now be able to `crank run` successfully!
 
 If you want an example of an independent project following these conventions, go check out [Nine Lives](https://github.com/bravely/nine_lives).
+
+## Updating Bindings
+
+If there's a newer [Playdate SDK](https://play.date/dev/) available that updates the C API, the crankstart bindings should be updated to match.
+Here's a guide.
+
+1. Install [the dependencies for bindgen](https://rust-lang.github.io/rust-bindgen/requirements.html).
+2. Install [bindgen-cli](https://rust-lang.github.io/rust-bindgen/command-line-usage.html).
+3. Install the gcc-arm-none-eabi toolchain, either [manually](https://developer.arm.com/Tools%20and%20Software/GNU%20Toolchain) or through a system package, which may also be named something like "cross-arm-none-eabi-gcc".
+4. On Linux, install the 32-bit glibc development package, which will be called something like `glibc-devel-32bit`.
+5. Install the new [Playdate SDK](https://play.date/dev/), and if it's not at the default MacOS path, set `PLAYDATE_SDK_PATH` to where you unzipped it.  (This should be the directory that contains `C_API`, `CoreLibs`, etc.)
+6. Run `./scripts/generate_bindings.sh`
+7. Inspect the changes to `crankstart-sys/src/bindings_*` - they should reflect the updates to the Playdate C API.  If nothing changed, double-check that the C API actually changed and not just the Lua API.
+8. Submit a PR with the changes :)
--- playdate/crankstart/src/sprite.rs
+++ playdate/crankstart/src/sprite.rs
@@ -97,8 +97,8 @@ impl<'a> Iterator for CollisionInfoIter<'a> {
             let sprite = sprite.unwrap();
             let other = other.unwrap();
             let collision_info = CollisionInfo {
-                sprite: sprite,
-                other: other,
+                sprite,
+                other,
                 info: &collision_slice[index],
             };
             Some(collision_info)
@@ -116,6 +116,7 @@ pub struct SpriteInner {
     pub raw_sprite: *mut crankstart_sys::LCDSprite,
     playdate_sprite: *const playdate_sprite,
     image: Option<Bitmap>,
+    userdata: Option<Rc<dyn core::any::Any>>,
 }
 
 pub type SpritePtr = Rc<RefCell<SpriteInner>>;
@@ -224,7 +225,7 @@ impl SpriteInner {
             (*self.playdate_sprite).setImage,
             self.raw_sprite,
             bitmap.inner.borrow().raw_bitmap,
-            flip.into()
+            flip,
         )?;
         self.image = Some(bitmap);
         Ok(())
@@ -236,6 +237,27 @@ impl SpriteInner {
 
     pub fn get_tag(&self) -> Result<u8, Error> {
         pd_func_caller!((*self.playdate_sprite).getTag, self.raw_sprite)
+    }
+
+    pub fn set_visible(&mut self, visible: bool) -> Result<(), Error> {
+        pd_func_caller!(
+            (*self.playdate_sprite).setVisible,
+            self.raw_sprite,
+            visible as i32
+        )
+    }
+
+    pub fn is_visible(&self) -> Result<bool, Error> {
+        let visible = pd_func_caller!((*self.playdate_sprite).isVisible, self.raw_sprite)?;
+        Ok(visible != 0)
+    }
+
+    pub fn set_opaque(&self, opaque: bool) -> Result<(), Error> {
+        pd_func_caller!(
+            (*self.playdate_sprite).setOpaque,
+            self.raw_sprite,
+            opaque as i32
+        )
     }
 
     pub fn move_to(&mut self, x: f32, y: f32) -> Result<(), Error> {
@@ -284,6 +306,32 @@ impl SpriteInner {
 
     pub fn mark_dirty(&mut self) -> Result<(), Error> {
         pd_func_caller!((*self.playdate_sprite).markDirty, self.raw_sprite,)
+    }
+
+    pub fn get_userdata<T>(&self) -> Result<Option<Rc<T>>, Error>
+    where
+        T: 'static,
+    {
+        self.userdata
+            .as_ref()
+            .map(
+                |userdata: &Rc<dyn core::any::Any>| -> Result<Rc<T>, Error> {
+                    userdata.clone().downcast::<T>().map_err(|err| {
+                        anyhow!(
+                            "Failed to cast userdata type {}",
+                            core::any::type_name::<T>(),
+                        )
+                    })
+                },
+            )
+            .transpose()
+    }
+
+    pub fn set_userdata<T>(&mut self, userdata: Rc<T>)
+    where
+        T: 'static,
+    {
+        self.userdata = Some(userdata);
     }
 }
 
@@ -396,6 +444,24 @@ impl Sprite {
             .move_to(x, y)
     }
 
+    pub fn set_visible(&mut self, visible: bool) -> Result<(), Error> {
+        self.inner
+            .try_borrow_mut()
+            .map_err(Error::msg)?
+            .set_visible(visible)
+    }
+
+    pub fn is_visible(&self) -> Result<bool, Error> {
+        self.inner.try_borrow().map_err(Error::msg)?.is_visible()
+    }
+
+    pub fn set_opaque(&self, opaque: bool) -> Result<(), Error> {
+        self.inner
+            .try_borrow_mut()
+            .map_err(Error::msg)?
+            .set_opaque(opaque)
+    }
+
     pub fn get_position(&self) -> Result<(f32, f32), Error> {
         self.inner.try_borrow().map_err(Error::msg)?.get_position()
     }
@@ -423,6 +489,20 @@ impl Sprite {
             .try_borrow_mut()
             .map_err(Error::msg)?
             .mark_dirty()
+    }
+
+    pub fn get_userdata<T>(&self) -> Result<Option<Rc<T>>, Error>
+    where
+        T: 'static,
+    {
+        self.inner.borrow().get_userdata()
+    }
+
+    pub fn set_userdata<T>(&mut self, userdata: Rc<T>)
+    where
+        T: 'static,
+    {
+        self.inner.borrow_mut().set_userdata(userdata)
     }
 }
 
@@ -472,13 +552,14 @@ impl SpriteManager {
 
     pub fn new_sprite(&mut self) -> Result<Sprite, Error> {
         let raw_sprite = pd_func_caller!((*self.playdate_sprite).newSprite)?;
-        if raw_sprite == core::ptr::null_mut() {
+        if raw_sprite.is_null() {
             Err(anyhow!("new sprite failed"))
         } else {
             let sprite = SpriteInner {
                 raw_sprite,
                 playdate_sprite: self.playdate_sprite,
                 image: None,
+                userdata: None,
             };
             sprite.set_update_function(unsafe { SPRITE_UPDATE.expect("SPRITE_UPDATE") })?;
             let sprite_ptr = Rc::new(RefCell::new(sprite));
@@ -495,6 +576,17 @@ impl SpriteManager {
         )
     }
 
+    pub fn get_sprite_count(&self) -> Result<i32, Error> {
+        pd_func_caller!((*self.playdate_sprite).getSpriteCount)
+    }
+
+    pub fn remove_sprite(&mut self, sprite: &Sprite) -> Result<(), Error> {
+        pd_func_caller!(
+            (*self.playdate_sprite).removeSprite,
+            sprite.inner.borrow_mut().raw_sprite
+        )
+    }
+
     pub fn add_dirty_rect(dirty_rect: LCDRect) -> Result<(), Error> {
         pd_func_caller!((*Self::get_mut().playdate_sprite).addDirtyRect, dirty_rect)
     }
@@ -507,10 +599,8 @@ impl SpriteManager {
         let weak_sprite = self.sprites.get(&raw_sprite);
         weak_sprite
             .and_then(|weak_sprite| weak_sprite.upgrade())
-            .and_then(|inner_ptr| {
-                Some(Sprite {
-                    inner: inner_ptr.clone(),
-                })
+            .map(|inner_ptr| Sprite {
+                inner: inner_ptr.clone(),
             })
     }
 
@@ -553,9 +643,9 @@ impl TextSprite {
 
         let width = graphics.get_system_text_width(text, tracking)?;
 
-        let mut text_bitmap =
+        let text_bitmap =
             graphics.new_bitmap(size2(width, SYSTEM_FONT_HEIGHT), background.clone())?;
-        graphics.with_context(&mut text_bitmap, || {
+        graphics.with_context(&text_bitmap, || {
             graphics.draw_text(text, point2(0, 0))?;
             Ok(())
         })?;
@@ -589,9 +679,9 @@ impl TextSprite {
 
         let width = graphics.get_system_text_width(text, tracking)?;
 
-        let mut text_bitmap =
+        let text_bitmap =
             graphics.new_bitmap(size2(width, SYSTEM_FONT_HEIGHT), self.background.clone())?;
-        graphics.with_context(&mut text_bitmap, || {
+        graphics.with_context(&text_bitmap, || {
             graphics.draw_text(text, point2(0, 0))?;
             Ok(())
         })?;
