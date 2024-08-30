930375c94997ca7cc705e15dd8914cd1f8214d78
Giorgio Pomettini <giorgio.freespire@gmail.com>
2024-08-30 18:59:07 +0200

    Made library more safe and it passes flags checks

--- playdate/src/lib.rs
+++ playdate/src/lib.rs
@@ -3,7 +3,7 @@
 extern crate alloc;
 extern crate pachip8risu;
 
-use crankstart::geometry::ScreenRect;
+use crankstart::{geometry::ScreenRect, log_to_console};
 use crankstart_sys::PDButtons;
 use euclid::{Point2D, Size2D};
 use pachip8risu::Chip8;
@@ -21,7 +21,10 @@ use {
 
 const WIDTH: i32 = 64;
 const HEIGHT: i32 = 32;
+const WIDTH_HIRES: i32 = 128;
+const HEIGHT_HIRES: i32 = 64;
 const SCALE: i32 = 6;
+const SCALE_HIRES: i32 = 3;
 
 struct State {
     cpu: Chip8,
@@ -42,7 +45,7 @@ macro_rules! BLACK {
 impl State {
     pub fn new(_playdate: &Playdate) -> Result<Box<Self>, Error> {
         let mut cpu = Chip8::new();
-        cpu.load_rom(include_bytes!("../../roms/test-opcode.rom"), Some(10));
+        cpu.load_rom(include_bytes!("../../roms/flags.rom"), Some(10));
 
         let (_, ms) = System::get().get_seconds_since_epoch().unwrap();
         cpu.set_random_seed(ms as u64);
@@ -55,7 +58,12 @@ impl Game for State {
     fn update(&mut self, _playdate: &mut Playdate) -> Result<(), Error> {
         System::get().draw_fps(0, 0)?;
 
-        self.cpu.update();
+        match self.cpu.update() {
+            Ok(()) => {}
+            Err(e) => {
+                log_to_console!("{}", e);
+            }
+        }
 
         let (_, pressed, released) = System::get().get_button_state().unwrap();
 
@@ -73,16 +81,27 @@ impl Game for State {
             _ => (),
         }
 
-        if let Some(gfx_buffer) = self.cpu.draw() {
-            for p in 0..WIDTH * HEIGHT {
-                let x = 8 + (p % 64) * SCALE;
-                let y = 24 + (p / 64) * SCALE;
-                Graphics::get()
-                    .fill_rect(
+        if !self.cpu.is_hi_res() {
+            if let Some(gfx_buffer) = self.cpu.draw() {
+                for p in 0..WIDTH * HEIGHT {
+                    let x = 8 + (p % 64) * SCALE;
+                    let y = 24 + (p / 64) * SCALE;
+                    Graphics::get().fill_rect(
                         ScreenRect::new(Point2D::new(x, y), Size2D::new(SCALE, SCALE)),
                         draw_pixel_color(gfx_buffer[p as usize]),
-                    )
-                    .unwrap();
+                    )?;
+                }
+            }
+        } else {
+            if let Some(gfx_buffer) = self.cpu.draw() {
+                for p in 0..WIDTH_HIRES * HEIGHT_HIRES {
+                    let x = 8 + (p % 128) * SCALE_HIRES;
+                    let y = 24 + (p / 128) * SCALE_HIRES;
+                    Graphics::get().fill_rect(
+                        ScreenRect::new(Point2D::new(x, y), Size2D::new(SCALE_HIRES, SCALE_HIRES)),
+                        draw_pixel_color(gfx_buffer[p as usize]),
+                    )?;
+                }
             }
         }
 
