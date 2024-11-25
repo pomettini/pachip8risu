ebf68342ac3eece8351fc510958b07f0328a4b04
Giorgio Pomettini <giorgio.freespire@gmail.com>
2024-11-26 00:22:29 +0100

    Migrated from crankstart to playdate library

--- playdate/src/lib.rs
+++ playdate/src/lib.rs
@@ -1,23 +1,25 @@
 #![no_std]
-
-extern crate alloc;
 extern crate pachip8risu;
 
-use crankstart::{geometry::ScreenRect, log_to_console};
-use crankstart_sys::PDButtons;
-use euclid::{Point2D, Size2D};
-use pachip8risu::Chip8;
+extern crate alloc;
 
-use {
-    alloc::boxed::Box,
-    anyhow::Error,
-    crankstart::{
-        crankstart_game,
-        graphics::{Graphics, LCDColor, LCDSolidColor},
-        system::System,
-        Game, Playdate,
-    },
-};
+#[macro_use]
+extern crate playdate as pd;
+extern crate playdate_controls as controls;
+
+use controls::buttons::PDButtonsExt;
+use controls::peripherals::Buttons;
+use pachip8risu::Chip8;
+use pd::sys::ffi::LCDColor;
+
+use core::ptr::NonNull;
+use pd::display::Display;
+use pd::graphics::bitmap::*;
+use pd::graphics::*;
+use pd::sys::ffi::PlaydateAPI;
+use pd::sys::EventLoopCtrl;
+use pd::system::prelude::*;
+use pd::system::update::UpdateCtrl;
 
 const WIDTH: i32 = 64;
 const HEIGHT: i32 = 32;
@@ -30,61 +32,57 @@ struct State {
     cpu: Chip8,
 }
 
-macro_rules! WHITE {
-    () => {
-        LCDColor::Solid(LCDSolidColor::kColorWhite)
-    };
-}
-
-macro_rules! BLACK {
-    () => {
-        LCDColor::Solid(LCDSolidColor::kColorBlack)
-    };
-}
-
 impl State {
-    pub fn new(_playdate: &Playdate) -> Result<Box<Self>, Error> {
+    fn new() -> Self {
+        // TODO: Init the state
+
         let mut cpu = Chip8::new();
         cpu.load_rom(include_bytes!("../../roms/scrolling.rom"), Some(10));
 
-        let (_, ms) = System::get().get_seconds_since_epoch().unwrap();
+        let ms = System::Cached().seconds_since_epoch();
         cpu.set_random_seed(ms as u64);
 
-        Ok(Box::new(Self { cpu }))
+        Self { cpu }
+    }
+
+    /// System event handler
+    fn event(&'static mut self, event: SystemEvent) -> EventLoopCtrl {
+        if let SystemEvent::Init = event {
+            Display::Default().set_refresh_rate(50.0);
+
+            // Register our update handler that defined below
+            self.set_update_handler();
+        }
+        EventLoopCtrl::Continue
     }
 }
 
-impl Game for State {
-    fn update(&mut self, _playdate: &mut Playdate) -> Result<(), Error> {
-        System::get().draw_fps(0, 0)?;
+impl Update for State {
+    /// Updates the state
+    fn update(&mut self) -> UpdateCtrl {
+        System::Cached().draw_fps(0, 0);
 
         match self.cpu.update() {
             Ok(()) => {}
             Err(e) => {
-                log_to_console!("{}", e);
+                println!("{}", e);
             }
         }
 
         // log_to_console!("{0:#04X}", self.cpu.get_opcode());
 
-        let (_, pressed, released) = System::get().get_button_state().unwrap();
+        let buttons = Buttons::Cached();
 
-        match pressed {
-            PDButtons::kButtonA => {
-                for i in 0..16 {
-                    self.cpu.keys[i] = true;
-                }
+        if buttons.pushed().a() {
+            for i in 0..16 {
+                self.cpu.keys[i] = true;
             }
-            _ => (),
         }
 
-        match released {
-            PDButtons::kButtonA => {
-                for i in 0..16 {
-                    self.cpu.keys[i] = false;
-                }
+        if buttons.released().a() {
+            for i in 0..16 {
+                self.cpu.keys[i] = false;
             }
-            _ => (),
         }
 
         if !self.cpu.is_hi_res() {
@@ -92,10 +90,13 @@ impl Game for State {
                 for p in 0..WIDTH * HEIGHT {
                     let x = 8 + (p % 64) * SCALE;
                     let y = 24 + (p / 64) * SCALE;
-                    Graphics::get().fill_rect(
-                        ScreenRect::new(Point2D::new(x, y), Size2D::new(SCALE, SCALE)),
+                    Graphics::Cached().fill_rect(
+                        x,
+                        y,
+                        SCALE,
+                        SCALE,
                         draw_pixel_color(gfx_buffer[p as usize]),
-                    )?;
+                    );
                 }
             }
         } else {
@@ -103,10 +104,13 @@ impl Game for State {
                 for p in 0..WIDTH_HIRES * HEIGHT_HIRES {
                     let x = 8 + (p % 128) * SCALE_HIRES;
                     let y = 24 + (p / 128) * SCALE_HIRES;
-                    Graphics::get().fill_rect(
-                        ScreenRect::new(Point2D::new(x, y), Size2D::new(SCALE_HIRES, SCALE_HIRES)),
+                    Graphics::Cached().fill_rect(
+                        x,
+                        y,
+                        SCALE_HIRES,
+                        SCALE_HIRES,
                         draw_pixel_color(gfx_buffer[p as usize]),
-                    )?;
+                    );
                 }
             }
         }
@@ -115,16 +119,35 @@ impl Game for State {
             // TODO: Add beep
         }
 
-        Ok(())
+        UpdateCtrl::Continue
     }
+}
+
+#[no_mangle]
+pub fn event_handler(
+    _api: NonNull<PlaydateAPI>,
+    event: SystemEvent,
+    _sim_key_code: u32,
+) -> EventLoopCtrl {
+    // Unsafe static storage for our state.
+    // Usually it's safe because there's only one thread.
+    pub static mut STATE: Option<State> = None;
+    if unsafe { STATE.is_none() } {
+        let state = State::new();
+        unsafe { STATE = Some(state) }
+    }
+
+    // Call state.event
+    unsafe { STATE.as_mut().expect("impossible") }.event(event)
 }
 
 const fn draw_pixel_color(is_on: bool) -> LCDColor {
     if is_on {
-        BLACK!()
+        LCDColorConst::BLACK
     } else {
-        WHITE!()
+        LCDColorConst::WHITE
     }
 }
 
-crankstart_game!(State);
+// Needed for debug build, absolutely optional
+ll_symbols!();
