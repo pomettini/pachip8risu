41e37a81e06adafea4d85c0e7c9f6bd0b7996f12
Giorgio Pomettini <giorgio.freespire@gmail.com>
2024-12-31 04:28:25 +0100

    Improved rendering performance, needs tweaks

--- playdate/src/lib.rs
+++ playdate/src/lib.rs
@@ -10,7 +10,7 @@ extern crate playdate_controls as controls;
 use controls::buttons::PDButtonsExt;
 use controls::peripherals::Buttons;
 use pachip8risu::Chip8;
-use pd::sys::ffi::LCDColor;
+use pd::sys::ffi::{LCDColor, LCD_COLUMNS, LCD_ROWS, LCD_ROWSIZE};
 
 use core::ptr::NonNull;
 use pd::display::Display;
@@ -37,7 +37,7 @@ impl State {
         // TODO: Init the state
 
         let mut cpu = Chip8::new();
-        cpu.load_rom(include_bytes!("../../roms/breakout.ch8"), Some(10));
+        cpu.load_rom(include_bytes!("../../roms/sweetcopter.ch8"), Some(160));
 
         let ms = System::Cached().seconds_since_epoch();
         cpu.set_random_seed(ms as u64);
@@ -48,7 +48,7 @@ impl State {
     /// System event handler
     fn event(&'static mut self, event: SystemEvent) -> EventLoopCtrl {
         if let SystemEvent::Init = event {
-            Display::Default().set_refresh_rate(30.0);
+            Display::Default().set_refresh_rate(50.0);
 
             // Register our update handler that defined below
             self.set_update_handler();
@@ -60,16 +60,7 @@ impl State {
 impl Update for State {
     /// Updates the state
     fn update(&mut self) -> UpdateCtrl {
-        System::Cached().draw_fps(0, 0);
-
-        match self.cpu.update() {
-            Ok(()) => {}
-            Err(e) => {
-                println!("{}", e);
-            }
-        }
-
-        // println!("{0:#04X}", self.cpu.get_opcode());
+        let graphics = Graphics::Cached();
 
         let buttons = Buttons::Cached();
 
@@ -93,12 +84,20 @@ impl Update for State {
             self.cpu.keys[2] = false;
         }
 
+        match self.cpu.update() {
+            Ok(()) => {}
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+
+        /*
         if self.cpu.should_draw {
             if !self.cpu.is_hi_res() {
                 for p in 0..WIDTH * HEIGHT {
                     let x = 8 + (p % 64) * SCALE;
                     let y = 24 + (p / 64) * SCALE;
-                    Graphics::Cached().fill_rect(
+                    graphics.fill_rect(
                         x,
                         y,
                         SCALE,
@@ -110,7 +109,7 @@ impl Update for State {
                 for p in 0..WIDTH_HIRES * HEIGHT_HIRES {
                     let x = 8 + (p % 128) * SCALE_HIRES;
                     let y = 24 + (p / 128) * SCALE_HIRES;
-                    Graphics::Cached().fill_rect(
+                    graphics.fill_rect(
                         x,
                         y,
                         SCALE_HIRES,
@@ -120,20 +119,36 @@ impl Update for State {
                 }
             }
         }
+        */
 
         if self.cpu.play_sound() {
             // TODO: Add beep
         }
 
-        UpdateCtrl::Continue
-    }
-}
+        let frame = graphics.get_frame().unwrap();
 
-const fn draw_pixel_color(is_on: bool) -> LCDColor {
-    if is_on {
-        LCDColorConst::BLACK
-    } else {
-        LCDColorConst::WHITE
+        let mut i = 0;
+        loop {
+            let start = i * 8;
+            let end = (i + 1) * 8;
+            frame[i + ((i / 16) * 36)] = draw_row(&self.cpu.gfx_buffer[start..end]);
+
+            i += 1;
+
+            if i >= ((LCD_ROWSIZE * LCD_ROWS) - 1) as usize {
+                break;
+            }
+
+            if i >= 1024 {
+                break;
+            }
+        }
+
+        graphics.mark_updated_rows(0, LCD_ROWS as i32 - 1);
+
+        System::Cached().draw_fps(0, 0);
+
+        UpdateCtrl::Continue
     }
 }
 
@@ -153,6 +168,14 @@ pub fn event_handler(
 
     // Call state.event
     unsafe { STATE.as_mut().expect("impossible") }.event(event)
+}
+
+pub fn draw_row(input: &[bool]) -> u8 {
+    input
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (i, b)| acc | (!b as u8) << i)
 }
 
 // Needed for debug build, absolutely optional
