3029ee340da84c32602259738385f2ae2d1487e3
Giorgio Pomettini <giorgio.freespire@gmail.com>
2025-01-02 18:29:31 +0100

    Added button mapping and a way to benchmark cpu/gpu

--- playdate/src/lib.rs
+++ playdate/src/lib.rs
@@ -1,18 +1,19 @@
 #![no_std]
 extern crate pachip8risu;
 
+#[macro_use]
 extern crate alloc;
 
 #[macro_use]
 extern crate playdate as pd;
 extern crate playdate_controls as controls;
 
+use api::Cache;
 use controls::buttons::PDButtonsExt;
 use controls::peripherals::Buttons;
 use pachip8risu::Chip8;
 use pd::sys::ffi::LCD_ROWSIZE;
 
-use core::iter::Scan;
 use core::ptr::NonNull;
 use pd::display::Display;
 use pd::graphics::*;
@@ -30,7 +31,7 @@ impl State {
         // TODO: Init the state
 
         let mut cpu = Chip8::new();
-        cpu.load_rom(include_bytes!("../../roms/sweetcopter.ch8"), Some(160));
+        cpu.load_rom(include_bytes!("../../roms/piper.ch8"), Some(160));
 
         let ms = System::Cached().seconds_since_epoch();
         cpu.set_random_seed(ms as u64);
@@ -54,28 +55,50 @@ impl Update for State {
     /// Updates the state
     fn update(&mut self) -> UpdateCtrl {
         let graphics = Graphics::Cached();
-
         let buttons = Buttons::Cached();
+        let system = System::Cached();
 
+        let (up, right, down, left, a, b) = (5, 9, 8, 7, 6, 10);
+
+        if buttons.pushed().up() {
+            self.cpu.keys[up] = true;
+        }
+        if buttons.pushed().right() {
+            self.cpu.keys[right] = true;
+        }
+        if buttons.pushed().down() {
+            self.cpu.keys[down] = true;
+        }
+        if buttons.pushed().left() {
+            self.cpu.keys[left] = true;
+        }
         if buttons.pushed().a() {
-            for i in 0..15 {
-                self.cpu.keys[i] = true;
-            }
+            self.cpu.keys[a] = true;
         }
-
         if buttons.pushed().b() {
-            self.cpu.keys[2] = true;
+            self.cpu.keys[b] = true;
         }
 
+        if buttons.released().up() {
+            self.cpu.keys[up] = false;
+        }
+        if buttons.released().right() {
+            self.cpu.keys[right] = false;
+        }
+        if buttons.released().down() {
+            self.cpu.keys[down] = false;
+        }
+        if buttons.released().left() {
+            self.cpu.keys[left] = false;
+        }
         if buttons.released().a() {
-            for i in 0..15 {
-                self.cpu.keys[i] = false;
-            }
+            self.cpu.keys[a] = false;
+        }
+        if buttons.released().b() {
+            self.cpu.keys[b] = false;
         }
 
-        if buttons.released().b() {
-            self.cpu.keys[2] = false;
-        }
+        let cpu_start = system.seconds_since_epoch_with_ms().1;
 
         match self.cpu.update() {
             Ok(()) => {}
@@ -84,55 +107,73 @@ impl Update for State {
             }
         }
 
+        let cpu_time = system.seconds_since_epoch_with_ms().1 - cpu_start;
+
         if self.cpu.play_sound() {
             // TODO: Add beep
         }
 
-        let frame = graphics.get_frame().unwrap();
+        let scale = if self.cpu.is_hi_res() { 3 } else { 6 };
+        let lcd_width = if self.cpu.is_hi_res() { 128 } else { 64 };
+        let lcd_height = if self.cpu.is_hi_res() { 64 } else { 32 };
 
-        const SCALE: usize = 5;
-        const LCD_WIDTH: usize = 64;
-        const LCD_HEIGHT: usize = 32;
+        let gpu_start = system.seconds_since_epoch_with_ms().1;
 
-        if self.cpu.should_draw {
-            for y in 0..LCD_HEIGHT {
-                for x in 0..LCD_WIDTH {
-                    let pixel = !self.cpu.gfx_buffer[y * LCD_WIDTH + x];
+        draw(graphics, &mut self.cpu, scale, lcd_width, lcd_height);
 
-                    // Draw a 3x3 block for each pixel
-                    for sy in 0..SCALE {
-                        for sx in 0..SCALE {
-                            let scaled_x = x * SCALE + sx;
-                            let scaled_y = y * SCALE + sy;
+        let gpu_time = system.seconds_since_epoch_with_ms().1 - gpu_start;
 
-                            // Calculate the position in the frame
-                            let byte_index = (scaled_y * LCD_ROWSIZE as usize) + (scaled_x / 8);
-                            let bit_index = 7 - (scaled_x % 8); // Bit order is reversed in each byte
+        /*
+        graphics
+            .draw_text(format!("CPU: {},  GPU: {}!", cpu_time, gpu_time), 0, 0)
+            .unwrap();
+        */
 
-                            if pixel {
-                                frame[byte_index] |= 1 << bit_index; // Set the pixel
-                            } else {
-                                frame[byte_index] &= !(1 << bit_index); // Clear the pixel
-                            }
-                        }
-                    }
-                }
-            }
-
-            // println!("{}, {}", self.cpu.rows_start, self.cpu.rows_end);
-
-            graphics.mark_updated_rows(
-                (self.cpu.rows_start * SCALE as u8).into(),
-                (self.cpu.rows_end * SCALE as u8).into(),
-            );
-
-            self.cpu.rows_start = u8::MAX;
-            self.cpu.rows_end = 0;
-        }
+        // println!("CPU: {}, GPU: {}!", cpu_time, gpu_time);
 
         System::Cached().draw_fps(0, 0);
 
         UpdateCtrl::Continue
+    }
+}
+
+pub fn draw(graphics: Graphics<Cache>, cpu: &mut Chip8, scale: usize, width: usize, height: usize) {
+    let frame = graphics.get_frame().unwrap();
+
+    if cpu.should_draw {
+        for y in 0..height {
+            for x in 0..width {
+                let pixel = !cpu.gfx_buffer[y * width + x];
+
+                // Draw a 3x3 block for each pixel
+                for sy in 0..scale {
+                    for sx in 0..scale {
+                        let scaled_x = x * scale + sx;
+                        let scaled_y = y * scale + sy;
+
+                        // Calculate the position in the frame
+                        let byte_index = (scaled_y * LCD_ROWSIZE as usize) + (scaled_x / 8);
+                        let bit_index = 7 - (scaled_x % 8); // Bit order is reversed in each byte
+
+                        if pixel {
+                            frame[byte_index] |= 1 << bit_index; // Set the pixel
+                        } else {
+                            frame[byte_index] &= !(1 << bit_index); // Clear the pixel
+                        }
+                    }
+                }
+            }
+        }
+
+        // println!("{}, {}", self.cpu.rows_start, self.cpu.rows_end);
+
+        graphics.mark_updated_rows(
+            (cpu.rows_start * scale as u8).into(),
+            (cpu.rows_end * scale as u8).into(),
+        );
+
+        cpu.rows_start = u8::MAX;
+        cpu.rows_end = 0;
     }
 }
 
